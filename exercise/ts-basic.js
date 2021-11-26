8886ed964ad245e2f398cde80754f58f519272d0
Shiho <mia.can2021@gmail.com>
2021-11-25 19:19:19 -0800

    home work

--- exercise/ts-basic.js
+++ exercise/ts-basic.js
@@ -1,108 +1,81 @@
 // ⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇
 //   Exercise 1 – Basic Types
 // ⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈
-
 // Objectives: 
 // • Annotate primitive types, arrays, and 'any' types
 // • Identify when type checking happens
 // • Afterwards we will analyze transpiler output
-
-const exerciseOne = () => {
+var exerciseOne = function () {
     // ======== Exercise 1.1 ========
     // Instructions:
     // • Hover over red squigglies to inspect the TS errors.
     // • Hover over variables to inspect their types.
     // • Fix the error on line 18 by changing the value of pi to the expected type.
-   
-    let pi = '3.14159';
-    let tau = +pi * 2;
-    
-    console.log('[Exercise 1.1]', `${tau} is ${pi} times two.`);
-  
+    var pi = '3.14159';
+    var tau = +pi * 2;
+    console.log('[Exercise 1.1]', "".concat(tau, " is ").concat(pi, " times two."));
     // ======== Exercise 1.2 ========
     // Instructions:
     // • Inspect type of `pie`
     // • Add an explicit type annotation to `pie`
     // • Try assigning invalid types, for fun
-  
-    let pie: string;
+    var pie;
     pie = 'blueberry';
-  
-    console.log('[Exercise 1.2]', `I like to eat ${pie}-flavored pie.`);
-  
+    console.log('[Exercise 1.2]', "I like to eat ".concat(pie, "-flavored pie."));
     // ======== Exercise 1.3 ========
     // Instructions:
     // • Inspect the error, then fix it.
-  
-    let isMark: boolean = true;
+    var isMark = true;
     // isMark = false;
-  
-    console.log('[Exercise 1.3]', `${isMark ? 'Oh, hi Mark' : 'Who are you?'}`);
-  
+    console.log('[Exercise 1.3]', "".concat(isMark ? 'Oh, hi Mark' : 'Who are you?'));
     // ======== Exercise 1.4 ========
     // Instructions:
     // • Add type annotations (as explicit as possible)
     // • Fix errors (if applicable)
-  
-    const integer: (number|string) = 6;
-    const float: number = 6.66;
-    const hex: number = 0xf00d;
-    const binary: number = 0b1010011010;
-    const octal: number = 0o744;
-    const negZero: number = -0;
-    const actuallyNumber: number = NaN;
-    const largestNumber: number = Number.MAX_VALUE;
-    const mostBiglyNumber: number = Infinity;
-  
-    const members: any[] = [
-      integer,
-      float,
-      hex,
-      binary,
-      octal,
-      negZero,
-      actuallyNumber,
-      largestNumber,
-      mostBiglyNumber
+    var integer = 6;
+    var float = 6.66;
+    var hex = 0xf00d;
+    var binary = 666;
+    var octal = 484;
+    var negZero = -0;
+    var actuallyNumber = NaN;
+    var largestNumber = Number.MAX_VALUE;
+    var mostBiglyNumber = Infinity;
+    var members = [
+        integer,
+        float,
+        hex,
+        binary,
+        octal,
+        negZero,
+        actuallyNumber,
+        largestNumber,
+        mostBiglyNumber
     ];
-  
     members[0] = '12345';
-  
     console.log('[Exercise 1.4]', members);
-  
     // ======== Exercise 1.5 ========
     // Instructions:
     // • Add type annotations (as explicit as possible)
     // • Fix errors (if applicable)
-  
-    const sequence: number[] = Array.from(Array(10).keys());
-    const animals: string[] = ['pangolin', 'aardvark', 'echidna', 'binturong'];
-  const stringsAndNumbers: (string | number)[] = [1, 'one', 2, 'two', 3, 'three'];
-    const allMyArrays: any[] = [sequence, animals, stringsAndNumbers];
-  
+    var sequence = Array.from(Array(10).keys());
+    var animals = ['pangolin', 'aardvark', 'echidna', 'binturong'];
+    var stringsAndNumbers = [1, 'one', 2, 'two', 3, 'three'];
+    var allMyArrays = [sequence, animals, stringsAndNumbers];
     console.log('Exercise 1.5', allMyArrays);
-  
     // ======== Exercise 1.6 ========
     // Goal:
     // • Add type annotations (as explicit as possible)
     // • Fix errors (if applicable)
-  
     // We want to represent an inventoryItem as a structure where
     // the first entry is the item name and the second is the quantity
-  
-    const inventoryItem: [string, number]= ['fidget wibbit', 11];
-  
+    var inventoryItem = ['fidget wibbit', 11];
     // later we destructure it
-   
-    const [name, qty]:[string, number] = inventoryItem;
-  
-    const msg: string = addInventory(name, qty);
-  
+    var name = inventoryItem[0], qty = inventoryItem[1];
+    var msg = addInventory(name, qty);
     console.log('[Exercise 1.6]', msg);
-  
-    function addInventory(name: string, quantity: number): string {
-      return `Added ${quantity} ${name}s to inventory.`;
+    function addInventory(name, quantity) {
+        return "Added ".concat(quantity, " ").concat(name, "s to inventory.");
     }
-  }
-  
-  exerciseOne()
+};
+exerciseOne();
